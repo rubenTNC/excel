2c8be9fb1f20876db6a7ebbd47d777d239bf0c36
rubenTNC <rubenen@mail.ru>
2023-02-22 21:21:50 +0300

    styleles

--- src/index.js
+++ src/index.js
@@ -3,12 +3,5 @@ import './styles/index.scss';
 
 const app = document.getElementById('app');
 
-const greeting = 'Hello World';
 
-app
-  ? (app.innerHTML = `
-    <strong class="main-title">
-        ${greeting}. Webpack работает!
-    </strong>
-`)
-  : null;
+
